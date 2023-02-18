6e513c1110279b677ad7a799d95c370bd3488d5f
Raven <ravenjoven15@gmail.com>
2023-02-18 18:25:53 +0800

    Linear,Constraints and Relative

--- app/src/main/java/com/example/myapplication/MainActivity3.java
+++ app/src/main/java/com/example/myapplication/MainActivity3.java
@@ -3,11 +3,12 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
-public class SecondActivity extends AppCompatActivity {
+public class MainActivity3 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);        }
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
     }
+}
