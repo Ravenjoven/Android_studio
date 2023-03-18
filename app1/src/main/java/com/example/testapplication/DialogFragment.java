f949c8c1e18216e53070c43bd29bdebce869923b
raven <ravenjoven15@gmail.com>
2023-03-18 18:35:55 +0800

    DialogFragment

---
+++ app1/src/main/java/com/example/testapplication/DialogFragment.java
@@ -0,0 +1,22 @@
+package com.example.testapplication;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DialogFragment extends androidx.fragment.app.DialogFragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.dialogfragment, container, false);
+    }
+
+    }
--- app1/src/main/java/com/example/testapplication/MainActivity.java
+++ app1/src/main/java/com/example/testapplication/MainActivity.java
@@ -1,35 +1,78 @@
 package com.example.testapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
+import androidx.annotation.NonNull;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
-    private final String TAG = "MainActivity";
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate( Bundle savedInstanceState ){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.v(TAG, "This is in verbose log.");
-        Log.d(TAG, "This is in debug log.");
-        Log.i(TAG, "This is in info log.");
-        Log.w(TAG, "This is in warning log.");
-        Log.e(TAG, "This is in error log.");
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("GFG|| Action Bar");
 
-        Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.i(TAG, "Button clicked");
-                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-                startActivity(intent);
-            }
-        });
+        actionBar.setSubtitle("Design a custom Action Bar");
+        actionBar.setIcon(R.mipmap.ic_launcher);
+
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayUseLogoEnabled(true);
+
+
+
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+
+            case R.id.search:{
+             showAlertDialog();
+            break;
+            }
+            case R.id.refresh:{
+             DialogFragment dialogFragment=new DialogFragment();
+             dialogFragment.show(getSupportFragmentManager(),"My Fragment");
+            break;
+            }
+            case R.id.copy:{
+                Toast.makeText(this,"Copy Clicked", Toast.LENGTH_SHORT).show();
+                break;
+            }
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+    private void showAlertDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Alert!")
+                .setMessage("Danger you are failing!");
+        builder.setPositiveButton("I know", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+
+            }
+        }).show();
+    }
+
 }
